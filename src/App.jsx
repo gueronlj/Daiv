ceab440a307a42f0ee275a6c1be53dd2c619c4b6
Lorens Gueron <gueronlj@gmail.com>
2023-03-30 13:27:46 -0400

    displays list of openAi engines

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Configuration, OpenAIApi } from "openai";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [engineList, setEngineList] = useState([])
+  const apiKey = import.meta.env.VITE_API_KEY
+
+  const getResponse = async() => {
+    const config = new Configuration({
+      organization: "org-va28fstFhQB2azoU9GGIvxTa",
+      apiKey: apiKey
+    })
+    const openai = new OpenAIApi(config)
+    const response = await openai.listEngines()
+    setEngineList(response.data.data);
+    console.log(response.data.data);
+  }
+
+  useEffect(() => {
+    getResponse()
+  },[]);
 
   return (
     <div className="App">
       <h1>This is DAIV</h1>
+      {engineList.map((engine) => (
+        <li key={engine.id}>{engine.id}</li>
+      ))}
     </div>
   )
 }
